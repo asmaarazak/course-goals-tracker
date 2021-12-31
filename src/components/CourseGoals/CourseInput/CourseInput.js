921680b881c31c4e81772c4d9ecd094011a33c96
asmaa <asma.razak@softwareq.com>
2021-12-31 11:29:48 +0800

    Creating multiple components in a file. Adding Styled Components on a div. Using props to dynamically change component styles.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => props.invalid ? 'red' : 'black'};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+    background: ${props => props.invalid ? 'rgb(243, 201, 201)' : 'transparent'};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,11 +53,22 @@ const CourseInput = props => {
   };
 
   return (
+    /* --Approach 1 on how to easily changed styling classes dynamically--
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl className={!isValid && 'invalid'}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
-      </div>
+      </FormControl>
+      <Button type="submit">Add Goal</Button>
+    </form>
+    */
+
+    // --Approach 2 on how to easily changed styling classes dynamically (using props)--
+    <form onSubmit={formSubmitHandler}>
+      <FormControl invalid={!isValid}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
