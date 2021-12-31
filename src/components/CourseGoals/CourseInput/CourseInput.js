6ced90f69d4d7cdc9ec1dbdc535dd1aaed43c79e
asmaa <asma.razak@softwareq.com>
2021-12-31 13:22:21 +0800

    Implementing CSS Modules with dynamic styles and media queries

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,35 +1,6 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-
-const FormControl = styled.div`
-    margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? 'red' : 'black'};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-    background: ${props => props.invalid ? 'rgb(243, 201, 201)' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -65,10 +36,10 @@ const CourseInput = props => {
 
     // --Approach 2 on how to easily changed styling classes dynamically (using props)--
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} value={enteredValue}/>
-      </FormControl>
+        <input type="text" onChange={goalInputChangeHandler} value={enteredValue} />
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
